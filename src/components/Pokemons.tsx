bee10d0340ffc7fa833b31e205199369e41febc6
nabiloutaous <nabil.outaous@gmail.com>
2024-03-04 22:58:33 +0100

    feat: Implement loader and apply minor updates

--- src/components/Pokemons.tsx
+++ src/components/Pokemons.tsx
@@ -1,21 +1,22 @@
 import { useQueries, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { getPokemonByName } from './Pokemon'
 import PokemonCard from '@/components/PokemonCard'
-import { PokemonType } from '@/types/Pokemon'
+import { Pokemon } from '@/types/Pokemon'
 import { useState } from 'react'
 import SearchBar from '@/components/SearchBar'
+import { getPokemonByName } from '@/hooks/usePokemonDetails'
+import Loader from '@/components/Loader'
 
 export const Pokemons = () => {
   const [searchInput, setSearchInput] = useState<string>('')
-  const [filteredPokemons, setFilteredPokemons] = useState<PokemonType[]>([])
+  const [filteredPokemons, setFilteredPokemons] = useState<Pokemon[]>([])
 
   const { data: list, isPending } = useQuery({
     queryKey: ['pokemonsData'],
     queryFn: () => axios('https://pokeapi.co/api/v2/pokemon?limit=151').then(response => response.data.results),
   })
 
-  const pokemonsList = useQueries<PokemonType[], PokemonType[]>({
+  const pokemonsList = useQueries<Pokemon[], Pokemon[]>({
     queries:
       list?.map((pokemon: { name: string }) => {
         return {
@@ -26,8 +27,7 @@ export const Pokemons = () => {
       }) ?? [],
   })
 
-  const pokemons: PokemonType[] = pokemonsList?.map(pokemon => pokemon.data)
-
+  const pokemons: Pokemon[] = pokemonsList?.map(pokemon => pokemon.data)
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
     e.preventDefault()
@@ -40,14 +40,13 @@ export const Pokemons = () => {
     }
   }
 
-
-  if (isPending) return 'Loading'
+  if (isPending) return <Loader/>
 
   return (
     <div className="p-4">
       <SearchBar handleChange={handleChange} filteredPokemons={filteredPokemons} searchInput={searchInput} />
       <div className="flex flex-wrap justify-center gap-4">
-        {pokemons.map((pokemon: PokemonType) => {
+        {pokemons.map((pokemon: Pokemon) => {
           if (!pokemon) return null
           return (
             <PokemonCard
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { PokemonType } from '@/types/Pokemon'
+import { Pokemon } from '@/types/Pokemon'
 
 interface SearchBarProps {
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  filteredPokemons: PokemonType[];
+  filteredPokemons: Pokemon[];
   searchInput: string;
 }
 
@@ -21,7 +21,7 @@ export const SearchBar = ({handleChange, filteredPokemons, searchInput}: SearchB
       {filteredPokemons.length !== 0 && (
         <ul
           className="absolute w-full border-2 border-t-0 border-gray-300 rounded-b-lg bg-white max-h-60 overflow-y-auto z-50 ">
-          {filteredPokemons.map((pokemon: PokemonType) => (
+          {filteredPokemons.map((pokemon: Pokemon) => (
             <li key={pokemon?.id} className="px-10 py-2 border-b-2 hover:bg-gray-100">
               <Link href={`/pokemon?name=${pokemon?.name}`} className="flex flex-row items-center justify-between gap-6">
               <span>{pokemon.name.toUpperCase()}</span>
