ab933e384e855b823d75e38752133937c3a20385
nabiloutaous <nabil.outaous@gmail.com>
2024-03-04 22:39:48 +0100

    feat: new loader created

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+export const Loader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <img src={"pikachu-running.gif"} alt="Chargement..." />
+    </div>
+  )
+}
+
+export default Loader
